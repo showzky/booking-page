f1e90eaf51c416a67c0f3daffd7d12ce305ab544
Showzky <andre.tonning@gmail.com>
2026-01-14 19:23:33 +0100

    working date and time selection logic

--- booking-page/src/App.tsx
+++ booking-page/src/App.tsx
@@ -2,6 +2,7 @@ import styles from './App.module.css';
 import { useState } from 'react';
 function App() {
   const [dato, setDato] = useState('');
+  const [selectedTime, setSelectedTime] = useState('');
   const availableTimes = ["09:00", "11:00", "13:00", "15:00"];
   return (
     <div className={styles.App}>
@@ -10,8 +11,15 @@ function App() {
       <label>Velg dato:</label>
       <input type="date" value={dato} onChange={(e) => setDato(e.target.value)}/>
       {availableTimes.map((time) => 
-        <button key={time} className={styles.timeButton}>{time}</button>
+        
+        <button key={time}
+         className={styles.timeButton}
+         onClick={() => setSelectedTime(time)}>
+          
+          {time}
+        </button>
       )}
+      <p>Valgt: {dato} kl. {selectedTime}</p>
     </div>
   )
 }
