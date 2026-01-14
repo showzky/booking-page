c9eacd5338e1ce709ac19a7557afb60f1030e4cf
Showzky <andre.tonning@gmail.com>
2026-01-14 19:06:47 +0100

    added time buttons

--- booking-page/src/App.tsx
+++ booking-page/src/App.tsx
@@ -2,12 +2,16 @@ import styles from './App.module.css';
 import { useState } from 'react';
 function App() {
   const [dato, setDato] = useState('');
+  const availableTimes = ["09:00", "11:00", "13:00", "15:00"];
   return (
     <div className={styles.App}>
       <h1>Bestill time</h1>
 
       <label>Velg dato:</label>
       <input type="date" value={dato} onChange={(e) => setDato(e.target.value)}/>
+      {availableTimes.map((time) => 
+        <button key="time" className="styles.timeButton">{time}</button>
+      )}
     </div>
   )
 }
